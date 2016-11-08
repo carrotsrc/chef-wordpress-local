291739941678620d93eeec216b99318cd7941fe9
Charlie Fyvie-Gauld <cfg@zunautica.org>
2016-11-08 09:47:44 +0000

    Updated for ubuntu system

--- recipes/addinstall.rb
+++ recipes/addinstall.rb
@@ -1,25 +1,25 @@
 
-directory '/srv/www/vhosts/' << node[:wp][:sub] <<'.zni.lan' do 
+directory '/var/www/' << node[:wp][:sub] <<'.wp' do 
     action :create
     mode '0775'
-    owner 'nginx'
+    owner 'www-data'
     group 'ops'
 end
 
-directory '/srv/www/vhosts/' << node[:wp][:sub] <<'.zni.lan/www' do 
+directory '/var/www/' << node[:wp][:sub] <<'.wp/www' do 
     action :create
     mode '0775'
-    owner 'nginx'
+    owner 'www-data'
     group 'ops'
 end
 
-template '/etc/nginx/sites-available/'<< node[:wp][:sub] << '.zni.lan.conf' do
+template '/etc/nginx/sites-available/'<< node[:wp][:sub] << '.wp.conf' do
     source 'nginx.conf.erb'
 end
 
 ruby_block 'add_host' do
     block do
-	    line = '127.0.0.1 ' << node[:wp][:sub] << '.zni.lan'
+	    line = '127.0.0.1 ' << node[:wp][:sub] << '.wp'
 	    file = Chef::Util::FileEdit.new('/etc/hosts')
 	    file.insert_line_if_no_match(/#{line}/, line)
 	    file.write_file
@@ -27,41 +27,41 @@ ruby_block 'add_host' do
 end
 
 
-link '/etc/nginx/sites-enabled/'<< node[:wp][:sub] << '.zni.lan.conf' do
-    to '/etc/nginx/sites-available/'<< node[:wp][:sub] << '.zni.lan.conf' 
+link '/etc/nginx/sites-enabled/'<< node[:wp][:sub] << '.wp.conf' do
+    to '/etc/nginx/sites-available/'<< node[:wp][:sub] << '.wp.conf' 
 end
 
 
 
 execute 'unpack-wordpress' do
-    cwd '/srv/www/vhosts/' << node[:wp][:sub] << '.zni.lan/www/'
+    cwd '/var/www/' << node[:wp][:sub] << '.wp/www/'
     command 'tar -xf /home/cfg/Software/wordpress-4.6.1.tar.gz'
 end
 
 ruby_block 'rename-wordpress' do
     block do
-	::File.rename('/srv/www/vhosts/' << node[:wp][:sub] << '.zni.lan/www/wordpress','/srv/www/vhosts/' << node[:wp][:sub] << '.zni.lan/www/public_html')
+	::File.rename('/var/www/' << node[:wp][:sub] << '.wp/www/wordpress','/var/www/' << node[:wp][:sub] << '.wp/www/public_html')
     end
 end
 
-execute 'set-selinux-context' do
-    cwd '/srv/www/vhosts/' << node[:wp][:sub] << '.zni.lan'
-    command 'chcon -Rv --type=httpd_sys_rw_content_t www'
+#execute 'set-selinux-context' do
+#    cwd '/var/www/' << node[:wp][:sub] << '.wp'
+#    command 'chcon -Rv --type=httpd_sys_rw_content_t www'
     #command 'chcon -Rv --type=httpd_sys_content_t www'
-end
+#end
 
 execute 'set-public_permissions' do
-    cwd '/srv/www/vhosts/' << node[:wp][:sub] << '.zni.lan/www/'
+    cwd '/var/www/' << node[:wp][:sub] << '.wp/www/'
     command 'chmod -R 775 *'
 end
 
 execute 'set-owner' do
-    cwd '/srv/www/vhosts/' << node[:wp][:sub] << '.zni.lan'
-    command 'chown -R nginx *'
+    cwd '/var/www/' << node[:wp][:sub] << '.wp'
+    command 'chown -R www-data *'
 end
 
 execute 'set-group' do
-    cwd '/srv/www/vhosts/' << node[:wp][:sub] << '.zni.lan'
+    cwd '/var/www/' << node[:wp][:sub] << '.wp'
     command 'chgrp -R ops *'
 end
 
@@ -70,12 +70,12 @@ service 'nginx' do
 end
 
 execute 'database-create' do
-	command "echo \"CREATE DATABASE IF NOT EXISTS " << node[:wp][:sub] << "_wpinst DEFAULT CHARACTER SET utf8; GRANT ALL PRIVILEGES ON  " << node[:wp][:sub] << "_wpinst . * TO dbagent_foo@localhost;\" | mysql --host=localhost --user=dbagent_foo";
+	command "echo \"CREATE DATABASE IF NOT EXISTS " << node[:wp][:sub].gsub("-","_") << "_wpinst DEFAULT CHARACTER SET utf8; GRANT ALL PRIVILEGES ON  " << node[:wp][:sub].gsub("-","_") << "_wpinst . * TO dbagent_foo@localhost;\" | mysql --host=localhost --user=dbagent_foo";
 end
 
 ruby_block 'add_subdomain_listing' do
     block do
-	    line = node[:wp][:sub] << '.zni.lan,/srv/www/vhosts/'<< node[:wp][:sub] << '.zni.lan/www/public_html'
+	    line = node[:wp][:sub] << '.wp,/var/www/'<< node[:wp][:sub] << '.wp/www/public_html'
 	    file = Chef::Util::FileEdit.new('/etc/nginx/subdomain.listing')
 	    file.insert_line_if_no_match(/#{line}/, line)
 	    file.write_file
@@ -83,9 +83,9 @@ ruby_block 'add_subdomain_listing' do
 end
 
 execute 'wordpress-database-config' do
-	command 'curl "http://' << node[:wp][:sub] << '.zni.lan/wp-admin/setup-config.php?step=2" --data "dbname='<< node[:wp][:sub] <<'_wpinst&uname=dbagent_foo&pwd&dbhost=localhost&prefix=wp_&language&submit=Submit"'
+	command 'curl "http://' << node[:wp][:sub] << '.wp/wp-admin/setup-config.php?step=2" --data "dbname='<< node[:wp][:sub].gsub("-","_") <<'_wpinst&uname=dbagent_foo&pwd&dbhost=localhost&prefix=wp_&language&submit=Submit"'
 end
 
 execute 'wordpress-install-config' do
-command 'curl "http://' << node[:wp][:sub] << '.zni.lan/wp-admin/install.php?step=2" --data "weblog_title=' << node[:wp][:sub] << ' Instance&user_name=admin&admin_password=passw0rd&pass1-text=passw0rd&admin_password2=passw0rd&admin_email=admin@foo.bar&Submit=Install+WordPress&language=en_US"'
+command 'curl "http://' << node[:wp][:sub] << '.wp/wp-admin/install.php?step=2" --data "weblog_title=' << node[:wp][:sub] << ' Instance&user_name=admin&admin_password=passw0rd&pass1-text=passw0rd&admin_password2=passw0rd&admin_email=admin@foo.bar&Submit=Install+WordPress&language=en_US"'
 end
--- recipes/reminstall.rb
+++ recipes/reminstall.rb
@@ -1,15 +1,15 @@
 
-link '/etc/nginx/sites-enabled/'<< node[:wp][:sub] << '.zni.lan.conf' do
+link '/etc/nginx/sites-enabled/'<< node[:wp][:sub] << '.wp.conf' do
     action :delete
 end
 
-file '/etc/nginx/sites-available/'<< node[:wp][:sub] << '.zni.lan.conf' do
+file '/etc/nginx/sites-available/'<< node[:wp][:sub] << '.wp.conf' do
     action :delete
 end
 
 
 
-directory '/srv/www/vhosts/' << node[:wp][:sub] << ".zni.lan" do
+directory '/var/www/' << node[:wp][:sub] << ".wp" do
     action :delete
     recursive true
     only_if{node[:wp][:sub] != ""}
@@ -18,7 +18,7 @@ end
 
 ruby_block 'rem_host' do
     block do
-	    line = '127.0.0.1 ' << node[:wp][:sub] << '.zni.lan'
+	    line = '127.0.0.1 ' << node[:wp][:sub] << '.wp'
 	    file = Chef::Util::FileEdit.new('/etc/hosts')
 	    file.search_file_delete_line(/#{line}/)
 	    file.write_file
@@ -27,7 +27,7 @@ end
 
 ruby_block 'rem_subdomain_listing' do
     block do
-	    line = node[:wp][:sub] << '.zni.lan,/srv/www/vhosts/'<< node[:wp][:sub] << '.zni.lan/www/public_html'
+	    line = node[:wp][:sub] << '.wp,/var/www/'<< node[:wp][:sub] << '.wp/www/public_html'
 	    file = Chef::Util::FileEdit.new('/etc/nginx/subdomain.listing')
 	    file.search_file_delete_line(/#{line}/)
 	    file.write_file
@@ -35,7 +35,7 @@ ruby_block 'rem_subdomain_listing' do
 end
 
 execute 'database-drop' do
-command "echo \"DROP DATABASE IF EXISTS " << node[:wp][:sub] << "_wpinst;\" | mysql --host=localhost --user=dbagent_foo";
+command "echo \"DROP DATABASE IF EXISTS " << node[:wp][:sub].gsub("-","_") << "_wpinst;\" | mysql --host=localhost --user=dbagent_foo";
 end
 
 service 'nginx' do
